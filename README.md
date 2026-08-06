701c14d16ea77536a336aedc592b5c0ddcc2e486
Wangai <jahkeyjohn@gmail.com>
2026-08-06 20:04:37 +0300

    Added base styles

--- README.md
+++ README.md
@@ -3,3 +3,23 @@
 The project is part of my frontend mentor journey. The objective of the exercise today is to understand forms. Data Collection and validation. I'll be using react-typescript and sass for styling.
 
 Data: I will be handling data in three phases: collection, validation and presentation.
+
+## challenge
+
+When decinding font-size, usually i have to think of text-presets ahead. The challenge then becomes deciding between text-preset as utility classes or mixins. When is the best use case for each. In this challenge, I have been provided text-preset and text-preset mobile thus i think mixin is the best option since i can use $media as argument and if
+
+```
+@mixin text-preset-1($media){
+    @if $media == "desktop"{
+        font-size: var(--fs-1)
+    }
+    @else if $media == "mobile"{
+        font-size: var(--fs-1-mobile)
+    }
+    font-weight: var(--fw-b);
+    line-height: var(--lh-1);
+}
+```
+
+otherwise I think i will end up with to many utility classes. If there is a better way, kindly advice.
+Second, Is there a better formula to generate clamp() for font-sizes or an industry standard?
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,5 +1,46 @@
+import Info from "../../assets/images/icon-info.svg";
+
 import "./form.scss";
 const Form = () => {
-  return <div>Form</div>;
+  return (
+    <form>
+      <div className="form-control">
+        <label htmlFor="avatar">Upload Avatar</label>
+        <input type="file" name="avatar" id="avatar" />
+        <span>
+          <img src={Info} alt="" />
+          Upload your photo (JPG or PNG, max size: 500KB).
+        </span>
+      </div>
+      <div className="form-control">
+        <label htmlFor="full-name">Full Name</label>
+        <input type="text" name="full-name" id="full-name" />
+        <span></span>
+      </div>
+      <div className="form-control">
+        <label htmlFor="email">Email Address</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="email@example.com"
+        />
+        <span></span>
+      </div>
+      <div className="form-control">
+        <label htmlFor="username">GitHub Username</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          placeholder="@yourusername"
+        />
+        <span></span>
+      </div>
+      <div className="form-control">
+        <button className="btn">Generate My Ticket</button>
+      </div>
+    </form>
+  );
 };
 export default Form;
